1ae1bc2fbedf65873e79cdc2594ea3496deb6be5
Karim <karim_skiy@mail.ru>
2023-04-28 11:15:21 +0300

    basic render of all days

---
+++ src/components/openedTaskComponent/OpenedTasksComponent.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react'
+import openedTasksStyles from './OpenedTasksComponent.module.scss'
+
+const OpenedTasks = (props) => {
+    const { openedTasks } = props;
+
+    const tasksForRender = openedTasks.map( (task) =>
+    <div className={openedTasksStyles.tasksCard} key={task.color + task.name + 222}>
+        <div className={task.color} key={task.name + 333 + task.color} />
+        <div 
+            className={openedTasksStyles.textOfOpenedTask}
+            key={task.name + 222 + task.color} 
+        >
+            <h3 key={task.description + task.name + 'j' + 0}>
+                {task.name}
+            </h3>
+            <p key={task.name + 'j' + task.description + 3}>
+                {task.description}
+            </p>
+        </div>
+            <label className='switch'>
+                <input type="checkbox" className='switchInput'/>
+                <span className='slider round'  onClick={handleSwitchClick}></span>
+            </label>
+    </div>
+)
+
+    function handleSwitchClick(e) {
+        const checkedText = e.target.parentNode.parentNode.children[1];
+        if (checkedText.style.textDecoration == 'line-through') {
+            checkedText.style.textDecoration = 'none'
+        } else {
+            checkedText.style.textDecoration = 'line-through'
+        }
+    }
+    
+    return (
+        <div className={openedTasksStyles.openedTasks}>
+            {tasksForRender}
+        </div>
+    )
+}
+
+export default OpenedTasks;
--- src/components/todayTasksComponent/TodayTasksComponent.js
+++ src/components/todayTasksComponent/TodayTasksComponent.js
@@ -1,40 +1,11 @@
 import React, { useState } from 'react'
 import todayTaskStyle from './TodayTasksComponent.module.scss';
+import OpenedTasks from '../openedTaskComponent/OpenedTasksComponent';
 
 const TodayTask = (props) => {
     const tasksForRender = props.dayForRender.arrayOfTasks;
     const [openedStatusClassName, setOpenedStatusClassName] = useState('openedStatusClassName');
     const [selectedDayForRender, setSelectedDayForRender] = useState(tasksForRender);
-
-    const openedTasks = selectedDayForRender.map( (task) =>
-        <div className={todayTaskStyle.tasksCard} key={task.color + task.name + 222}>
-            <div className={task.color} key={task.name + 333 + task.color} />
-            <div 
-                className={todayTaskStyle.textOfOpenedTask}
-                key={task.name + 222 + task.color} 
-            >
-                <h3 key={task.description + task.name + 'j' + 0}>
-                    {task.name}
-                </h3>
-                <p key={task.name + 'j' + task.description + 3}>
-                    {task.description}
-                </p>
-            </div>
-                <label className='switch'>
-                    <input type="checkbox" className='switchInput'/>
-                    <span className='slider round'  onClick={handleSwitchClick}></span>
-                </label>
-        </div>
-    )
-
-    function handleSwitchClick(e) {
-        const checkedText = e.target.parentNode.parentNode.children[1];
-        if (checkedText.style.textDecoration == 'line-through') {
-            checkedText.style.textDecoration = 'none'
-        } else {
-            checkedText.style.textDecoration = 'line-through'
-        }
-    }
 
     function handlerButtonClick() {
         if (openedStatusClassName == 'closedStatusClassName') {
@@ -55,7 +26,7 @@ const TodayTask = (props) => {
                 />
                 <h3>{props.dayForRender.title + " "}:</h3>
             </div>
-            <div className={todayTaskStyle.openedTasks}>{openedTasks}</div>
+            <OpenedTasks openedTasks = {selectedDayForRender}/>
         </div>
     )
 }
