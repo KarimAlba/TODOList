7b0260305b667abc0e9fd70e1d3b441095ebea62
Karim <karim_skiy@mail.ru>
2023-05-13 13:50:04 +0300

    fixed bags with alltaskscomponent rerender

--- src/components/taskComponent/TaskComponent.js
+++ src/components/taskComponent/TaskComponent.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo, useState } from 'react'
+import React, { useMemo } from 'react'
 import taskStyles from './TaskComponent.module.scss'
 
 const Task = (props) => {
